f15bd7b794a884e01f998fe60efc91ed30b7b2b0
nmduc <nmduc22112007@gmail.com>
2026-01-16 11:32:49 +0700

    Merge remote-tracking branch 'origin/main'

--- KIEM TRA CUOI HOC PHAN/bai 2.py
+++ KIEM TRA CUOI HOC PHAN/bai 2.py
@@ -1,9 +1,16 @@
-n = int(input("Nhap n(n > 0):"))
-if n <= 0:
-    n = int(input("Vui long nhap n > 0:"))
+
 def tong(n):
     if n < 10:
         return n
     else:
         return tong(n//10) + n%10
-print(tong(n))
+while True:
+    try:
+        n=int(input())
+        if n<0:
+            print("....")
+        else:
+            print(tong(n))
+            break
+    except ValueError:
+        print("khong hop le")
